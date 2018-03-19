c30e55a7e65c3780f4526bda568cf3242a304f1f
Albin <albin.larsson@raa.se>
2018-03-19 12:48:11 +0100

    multithreading for file import

--- import.py
+++ import.py
@@ -1,4 +1,5 @@
 import glob
+import time
 
 import click
 import requests
@@ -8,6 +9,8 @@ import background
 headers = {
     'Content-Type': 'application/rdf+xml',
 }
+
+count = 0
 
 def error(text):
     click.secho(text, err=True, fg='red')
@@ -19,17 +22,21 @@ def import_from_uri(uri, sparql_endpoint):
 def confirm_import_from_uris(data, sparql_endpoint):
     pass
 
+@background.task
 def import_from_file(filepath, sparql_endpoint):
-    rdf=None
+    global count
+    rdf = None
     with open(filepath, 'r', encoding='utf-8') as f:
         rdf = f.read()
-    click.secho('Importing: {} | '.format(filepath), fg='yellow', nl=False)
 
     r = requests.post(sparql_endpoint, data=rdf.encode('utf-8'), headers=headers)
-    if not 'modified' in r.text:
+    click.secho('Importing: {0} | '.format(filepath), fg='yellow', nl=False)
+    if not 'modified' in r.text or r.status_code is not 200:
         click.secho('Failed', fg='red')
     else:
         click.secho('Success', fg='green')
+
+    count += 1
 
 def confirm_imports_from_dir(path, sparql_endpoint):
     files = glob.glob(path + '/*.rdf')
@@ -42,6 +49,10 @@ def confirm_imports_from_dir(path, sparql_endpoint):
     if c == 'y':
         for f in files:
             import_from_file(f, sparql_endpoint)
+
+        # the sleep is used for performance reasons
+        while count < len(files):
+            time.sleep(0.5)
 
         click.secho('Done!', fg='green')
     exit()
